1ba40c3c6dae91d4d6b0d1ec3c23ee0b136e65d2
stopbrother <stopbro0323@naver.com>
2024-10-02 16:37:35 +0900

    add: 챔피언 목록페이지 구현

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,17 +1,28 @@
+import { ChampionListItem } from "@/types/Champion";
 import { getChampionList, getLatestVersion } from "@/utils/serverApi";
+import Image from "next/image";
+import Link from "next/link";
 
 export const revalidate = 86400;
 
-const Championspage = async () => {
+const ChampionsPage = async () => {
   const version = await getLatestVersion();
-  const champions = await getChampionList(version);
+  const champions: ChampionListItem[] = await getChampionList(version);
 
   return (
     <main>
       <ul>
         {champions?.map((champion) => (
-          <li key={champion.key}>
-            <p>{champion.name}</p>
+          <li key={champion.id}>
+            <Link href={`/champions/${champion.id}`}>
+              <Image
+                src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.image.full}`}
+                alt={champion.name}
+                width={100}
+                height={100}
+              />
+              <p>{champion.name}</p>
+            </Link>
           </li>
         ))}
       </ul>
@@ -19,4 +30,4 @@ const Championspage = async () => {
   );
 };
 
-export default Championspage;
+export default ChampionsPage;
--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { Champion, ChampionResponse } from "@/types/Champion";
+import { ChampionListItem, ChampionsData } from "@/types/Champion";
 
 export const getLatestVersion = async () => {
   try {
@@ -8,33 +8,26 @@ export const getLatestVersion = async () => {
       "https://ddragon.leagueoflegends.com/api/versions.json"
     );
 
-    const versions = await response.json();
+    const versions: string[] = await response.json();
     return versions[0];
   } catch (error) {
     console.error("fetch version:", error);
+    return "";
   }
 };
 
-export const getChampionList = async (version: string) => {
+export const getChampionList = async (
+  version: string
+): Promise<ChampionListItem[]> => {
   try {
     const response = await fetch(
       `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion.json`
     );
 
-    const data: ChampionResponse = await response.json();
+    const data: ChampionsData = await response.json();
     return Object.values(data.data);
   } catch (error) {
     console.error("fetch champions:", error);
+    return [];
   }
 };
-
-// export const getChampion = async ({id, version}: Champion["id", "version"]) => {
-//   try {
-//     const response = await fetch(
-//       `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion/${id}.json`
-//     );
-
-//     const data = await response.json();
-//     return data;
-//   } catch {}
-// };
