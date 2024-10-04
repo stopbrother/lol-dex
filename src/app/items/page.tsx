6520d999f8c9cbb6040525ca8e65e05f8d221162
stopbrother <stopbro0323@naver.com>
2024-10-04 15:43:38 +0900

    add: 아이템 목록 페이지

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,7 +1,33 @@
+import { Item } from "@/types/Item";
+import { fetchItemList, getLatestVersion } from "@/utils/serverApi";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Itemspage = () => {
-  return <div>page</div>;
+const Itemspage = async () => {
+  const version = await getLatestVersion();
+  const ItemList = await fetchItemList(version);
+  const Items = Object.entries(ItemList);
+
+  return (
+    <div>
+      <ul>
+        {Items.map(([id, item]: [string, Item]) => (
+          <li key={id}>
+            <Link href={`/items/${id}`}>
+              <Image
+                src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item.image.full}`}
+                alt={item.name}
+                width={100}
+                height={100}
+              />
+              <p>{item.name}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Itemspage;
