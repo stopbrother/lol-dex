e514873d9ff4f6c9fc71681ad1df68a6b61c357c
stopbrother <stopbro0323@naver.com>
2024-10-04 19:48:54 +0900

    update: route handler 에러추가 및 타입 선언

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,17 +1,26 @@
+import { ChampionRotation } from "@/types/ChampionRotation";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   const apiKey = process.env.RIOT_API_KEY as string;
 
-  const response = await fetch(
-    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
-    {
-      headers: {
-        "X-Riot-Token": apiKey,
-      },
-    }
-  );
+  if (!apiKey) throw new Error("API키가 없습니다.");
 
-  const data = await response.json();
-  return NextResponse.json(data);
+  try {
+    const response = await fetch(
+      "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
+      {
+        method: "GET",
+        headers: {
+          "X-Riot-Token": apiKey,
+        },
+      }
+    );
+
+    const data: ChampionRotation = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("챔피언 로테이션 데이터를 가져오는데 실패했습니다.", error);
+    return NextResponse.error();
+  }
 }
