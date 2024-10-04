a2ca27193e1a6cab210bc45b3eb3f946c544bbfa
stopbrother <stopbro0323@naver.com>
2024-10-04 14:04:51 +0900

    update: Item api 타입 정의

--- src/types/Item.ts
+++ src/types/Item.ts
@@ -1,5 +1,33 @@
+export interface ItemsData {
+  type: string;
+  version: string;
+  data: {
+    [key: string]: Item;
+  };
+}
 export interface Item {
   name: string;
+  description: string;
+  colloq: string;
   plaintext: string;
-  image: { sprite: string };
+  image: ItemImage;
+  gold: ItemGold;
+  stats: { FlatMovementSpeedMod: number };
+}
+
+export interface ItemImage {
+  full: string;
+  sprite: string;
+  group: string;
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
+export interface ItemGold {
+  base: number;
+  purchasable: boolean;
+  total: number;
+  sell: number;
 }
