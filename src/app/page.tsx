05b61a7cd9d7202cb2781a6cce54a16ab79d206f
stopbrother <stopbro0323@naver.com>
2024-10-07 16:16:56 +0900

    style: 헤더 & 홈 스타일 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <main className="w-full max-w-[1024px] flex justify-center items-center">
+      <div>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</div>
+    </main>
+  );
 };
 
 export default Homepage;
