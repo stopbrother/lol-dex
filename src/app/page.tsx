f79fa32733a38435684ac49bf9ba8c9e8e9d5ec5
stopbrother <stopbro0323@naver.com>
2024-10-07 18:01:01 +0900

    feat: 홈화면 스타일적용 및 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const Homepage = () => {
   return (
-    <main className="w-full max-w-[1024px] flex justify-center items-center">
+    <main className="w-full max-w-[1024px] flex flex-col justify-center items-center">
       <div>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</div>
+      <Link href="/champions">챔피언 목록</Link>
+      <Link href="/items">아이템 목록</Link>
+      <Link href="/rotations">로테이션 목록</Link>
     </main>
   );
 };
