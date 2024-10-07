0dfa3f514c427ad51f03b93d32bcff4b1d59e2bd
stopbrother <stopbro0323@naver.com>
2024-10-07 16:57:12 +0900

    style: 로테이션 목록페이지 style 추가

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -3,6 +3,8 @@ import { ChampionListItem } from "@/types/Champion";
 import { ChampionRotationIds } from "@/types/ChampionRotation";
 import { getChampionRotation } from "@/utils/riotApi";
 import { fetchChampionList, getLatestVersion } from "@/utils/serverApi";
+import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const Rotationpage = () => {
@@ -10,6 +12,7 @@ const Rotationpage = () => {
     null
   );
   const [champions, setChampions] = useState<ChampionListItem[]>([]);
+  const [version, setVersion] = useState<string>("");
 
   useEffect(() => {
     const fetchRoation = async () => {
@@ -17,6 +20,7 @@ const Rotationpage = () => {
       setRotationData(data);
 
       const version = await getLatestVersion();
+      setVersion(version);
 
       const championListData = await fetchChampionList(version);
       setChampions(championListData);
@@ -30,10 +34,18 @@ const Rotationpage = () => {
 
   return (
     <div>
-      <ul>
+      <ul className="flex flex-wrap gap-8 justify-center items-center">
         {rotationChampions?.map((champion) => (
           <li key={champion.key}>
-            <p>{champion.name}</p>
+            <Link href={`/champions/${champion.key}`}>
+              <Image
+                src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.image.full}`}
+                alt={champion.name}
+                width={100}
+                height={100}
+              />
+              <p className="text-center break-all">{champion.name}</p>
+            </Link>
           </li>
         ))}
       </ul>
