9d7f366537c2e566b9ee3eb768636210f6b5883a
stopbrother <stopbro0323@naver.com>
2024-10-08 10:50:58 +0900

    style: 홈화면 style 수정 max-w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <main className="w-full max-w-[1024px] flex flex-col justify-center items-center">
+    <main className="w-full flex flex-col justify-center items-center">
       <div>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</div>
       <Link href="/champions">챔피언 목록</Link>
       <Link href="/items">아이템 목록</Link>
